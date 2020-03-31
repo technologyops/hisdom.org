a4e6128e488de79cb0951e1fbe1bc95cf370839c
Technology Operations <inbox@techops.in>
2020-03-31 16:17:26 +0530

    first commit

---
+++ js/brain.js
@@ -0,0 +1,24 @@
+/* This file was automatically generated. You shouldn't
+ * need to edit this file by hand. If you need to add files
+ * to this list, instead run the "Refresh the File List"
+ * command in the RiveScript::HTTPd index page. */
+
+var rs_filelist = [
+	'admin.rive',
+	'begin.rive',
+	'clients.rive',
+	'culture.rive',
+	'eliza.rive',
+	'experience.rive',
+	'howtochangeman.rive',
+	'jk.rive',
+	'knowledge.rive',
+	'meditation.rive',
+	'myself.rive',
+	'religion.rive',
+	'resultofknowledge.rive',
+	'rpg.rive',
+	'test.rive',
+	'thought.rive',
+	'want.rive'
+];
---
+++ js/config.js
@@ -0,0 +1,19 @@
+// RiveScript::HTTPd JavaScript Client Settings
+var rs_settings = {
+
+/* The name your bot should appear as in the history window. */
+	'bot_name': 'JKrishnamurthi',
+
+/* A welcome message to be displayed immediately when the page loads */
+	/*'welcome': "Hi there! I'm a JK chatbot. I derive my knowledge from my master J.Krishnamurthi. How can I help you today?",*/
+    'welcome' : " I maintain that Truth is a pathless land, and you cannot approach it by any path whatsoever, by any religion, by any sect. That is my point of view, and I adhere to that absolutely and unconditionally. Truth, being limitless, unconditioned, unapproachable by any path whatsoever, cannot be organized; nor should any organization be formed to lead or to coerce people along any particular path.If you first understand that, then you will see how impossible it is to organize a belief. A belief is purely an individual matter, and you cannot and must not organize it. If you do, it becomes dead, crystallized; it becomes a creed, a sect, a religion, to be imposed on others. This is what everyone throughout the world is attempting to do. Truth is narrowed down and made a plaything for those who are weak, for those who are only momentarily discontented. Truth cannot be brought down, rather the individual must make the effort to ascend to it. You cannot bring the mountain-top to the valley. If you would attain to the mountain-top you must pass through the valley, climb the steeps, unafraid of the dangerous precipices. I maintain that no organization can lead man to spirituality. If an organization be created for this purpose of realisation of truth through a spiritual leader, it becomes a crutch, a weakness, a bondage, and must cripple the individual, and prevent him from growing, from establishing his uniqueness, which lies in the discovery for himself of that absolute, unconditioned Truth. ",
+
+
+/* The path to your RiveScript brain. On the demo web server, this is by default
+ * kept in the "brain" directory one level above the "js" one. When you upload
+ * this to your web host, you'll probably want to move it to be in the same
+ * folder as the HTML page. Change this value to be simply 'brain' in this case. */
+	'brain_path': '../brain'
+	// 'brain_path': 'brain'
+
+};
